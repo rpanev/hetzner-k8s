20ac3ddf6e889207de5600aa930dbcd1124c468c
Radoslav Panev <radoslav.panev@gmail.com>
2025-08-14 14:31:05 +0300

    fix: Implement LoadBalancer IP retrieval and display in demo app
    - Add Python sidecar container to serve dynamic pod/node/LB information
    - Fix nginx configuration to properly proxy requests to sidecar
    - Restructure deployment script to wait for LoadBalancer IP assignment
    - Use Hetzner Cloud CLI to get real LoadBalancer IP
    - Pass LoadBalancer IP as environment variable to containers
    - Fix endpoint responses for /hostname, /pod-ip, /node, and /lb-ip
    - Move deployment scripts to demo-deploy directory
    - Update README with LoadBalancer demo documentation

--- terrafrom/lb.tf
+++ terrafrom/lb.tf
@@ -1,78 +1,105 @@
-# # Load Balancer Configuration // TODO: Add load balancer configuration
-# resource "hcloud_load_balancer" "load_balancer" {
-#   name               = "${var.node}-load-balancer"
-#   load_balancer_type = var.load_balancer_type
-#   location           = var.lb_datacenter
-#   delete_protection  = true
-#   labels = {
-#     env       = "prod"
-#     terraform = "true"
-#     role      = "LoadBalancer"
-#   }
-#
-#   depends_on = [hcloud_network.kube_network, hcloud_server.kube_master]
-# }
+# Load Balancer Configuration
+resource "hcloud_load_balancer" "load_balancer" {
+  name               = "${var.node}-load-balancer"
+  load_balancer_type = var.load_balancer_type
+  location           = var.lb_datacenter
+  delete_protection  = var.delete_protection
+  labels = {
+    env       = "prod"
+    terraform = "true"
+    role      = "LoadBalancer"
+  }
 
-# Attach Load Balancer to Network // TODO: Add load balancer attachment
-# resource "hcloud_load_balancer_network" "lb_network" {
-#   load_balancer_id = hcloud_load_balancer.load_balancer.id
-#   network_id       = hcloud_network.kube_network.id
-#
-#   depends_on = [hcloud_network.kube_network, hcloud_load_balancer.load_balancer]
-# }
+  depends_on = [hcloud_network.kube_network, hcloud_server.kube_master]
+}
 
-# Attach Master Node to the Load Balancer // TODO: Add load balancer attachment
-# resource "hcloud_load_balancer_target" "load_balancer_target_master" {
-#   type             = "server"
-#   load_balancer_id = hcloud_load_balancer.load_balancer.id
-#   server_id        = hcloud_server.kube_master.id
-#   use_private_ip   = true
-#
-#   depends_on = [
-#     hcloud_network.kube_network,
-#     hcloud_load_balancer_network.lb_network
-#   ]
-# }
+# Attach Load Balancer to Network
+resource "hcloud_load_balancer_network" "lb_network" {
+  load_balancer_id = hcloud_load_balancer.load_balancer.id
+  network_id       = hcloud_network.kube_network.id
+
+  depends_on = [hcloud_network.kube_network, hcloud_load_balancer.load_balancer]
+}
+
+# Attach Master Node to the Load Balancer
+resource "hcloud_load_balancer_target" "load_balancer_target_master" {
+  type             = "server"
+  load_balancer_id = hcloud_load_balancer.load_balancer.id
+  server_id        = hcloud_server.kube_master.id
+  use_private_ip   = true
+
+  depends_on = [
+    hcloud_network.kube_network,
+    hcloud_load_balancer_network.lb_network
+  ]
+}
 
 
-# Configure HTTP Service // TODO: Add load balancer service
-# resource "hcloud_load_balancer_service" "http_service" {
-#   load_balancer_id = hcloud_load_balancer.load_balancer.id
-#   protocol         = "http"
-#   listen_port      = 80
-#   destination_port = 80
-#
-#   depends_on = [hcloud_load_balancer.load_balancer]
-# }
+# Configure HTTP Service
+resource "hcloud_load_balancer_service" "http_service" {
+  load_balancer_id = hcloud_load_balancer.load_balancer.id
+  protocol         = "http"
+  listen_port      = 80
+  destination_port = 80
 
-# # Configure HTTPS Service // TODO: Add load balancer service
+  health_check {
+    protocol = "http"
+    port     = 80
+    interval = 15
+    timeout  = 10
+    retries  = 3
+    http {
+      path = "/"
+      status_codes = ["2??"]
+    }
+  }
+
+  depends_on = [hcloud_load_balancer.load_balancer]
+}
+
+# # Configure HTTPS Service
+# # Забележка: За да използвате HTTPS, трябва да имате валиден SSL сертификат в Hetzner Cloud
+# # Закоментирайте този блок, ако нямате сертификат или променете certificates с вашия ID
 # resource "hcloud_load_balancer_service" "https_service" {
 #   load_balancer_id = hcloud_load_balancer.load_balancer.id
 #   protocol         = "https"
 #   listen_port      = 443
 #   destination_port = 443
-#
+
 #   http {
-#     certificates = [1385981]
+#     # Заменете с вашия сертификат ID или използвайте променлива
+#     certificates = []
 #   }
-#
+
+#   health_check {
+#     protocol = "https"
+#     port     = 443
+#     interval = 15
+#     timeout  = 10
+#     retries  = 3
+#     http {
+#       path = "/"
+#       status_codes = ["2??", "3??"]
+#     }
+#   }
+
 #   depends_on = [hcloud_load_balancer.load_balancer]
 # }
 
-# Configure Kubernetes API Service // TODO: Add load balancer service
-# resource "hcloud_load_balancer_service" "kubernetes_api_service" {
-#   load_balancer_id = hcloud_load_balancer.load_balancer.id
-#   protocol         = "tcp"
-#   listen_port      = 6443
-#   destination_port = 6443
-#
-#   health_check {
-#     protocol = "tcp"
-#     port     = 6443
-#     interval = 10
-#     timeout  = 5
-#     retries  = 3
-#   }
-#
-#   depends_on = [hcloud_load_balancer.load_balancer]
-# }
+# Configure Kubernetes API Service
+resource "hcloud_load_balancer_service" "kubernetes_api_service" {
+  load_balancer_id = hcloud_load_balancer.load_balancer.id
+  protocol         = "tcp"
+  listen_port      = 6443
+  destination_port = 6443
+
+  health_check {
+    protocol = "tcp"
+    port     = 6443
+    interval = 10
+    timeout  = 5
+    retries  = 3
+  }
+
+  depends_on = [hcloud_load_balancer.load_balancer]
+}
